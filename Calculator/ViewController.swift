fe1c30b28c2bd70cee052fa3041615dcffc06fc0
Akaev <akaev@MacBook-Pro-Akaev.local>
2023-08-20 23:59:18 +0600

    My first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Akaev on 20/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
