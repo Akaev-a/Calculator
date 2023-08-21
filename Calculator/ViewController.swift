95045141c009f64038c4726fb37a5e3e73110dc5
Akaev <akaev@MacBook-Pro-Akaev.local>
2023-08-22 00:05:40 +0600

    Govno kod from YouTube

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     
     var numberFromScreen: Double = 0
     var firstNumber: Double = 0
+    var operation: Int = 0
     var mathsSigh: Bool = false
     
     @IBAction func numberPress(_ sender: UIButton) {
@@ -25,40 +26,60 @@ class ViewController: UIViewController {
         numberFromScreen = Double(displayResultLabel.text!)!
     }
     @IBAction func buttons(_ sender: UIButton) {
-        if displayResultLabel.text != "" {
+        if displayResultLabel.text != "" && sender.tag != 10 && sender.tag != 11 && sender.tag != 17 && sender.tag != 19 {
             firstNumber = Double(displayResultLabel.text!)!
             if sender.tag == 10 { // C
                 
             }
             else if sender.tag == 11 { // +/-
-                
+                displayResultLabel.text = "+/-"
             }
             else if sender.tag == 12 { // %
-                
+                displayResultLabel.text = "%"
             }
             else if sender.tag == 13 { // +
                 displayResultLabel.text = "+"
                 
             }
             else if sender.tag == 14 { // -
-                
+                displayResultLabel.text = "-"
             }
             else if sender.tag == 15 { // x
-                
+                displayResultLabel.text = "×"
             }
             else if sender.tag == 16 { // /
-                
+                displayResultLabel.text = "÷"
             }
-            else if sender.tag == 17 { // =
-                
-            }
+            
             else if sender.tag == 18 { // koren`
-                
+                displayResultLabel.text = "√"
             }
             else if sender.tag == 19 { // .
-                
+                displayResultLabel.text = "."
             }
+        
             mathsSigh = true
+            operation = sender.tag
+        }
+        else if sender.tag == 17 { // =
+            if operation == 13 {
+                displayResultLabel.text = String(firstNumber + numberFromScreen)
+            }
+            else if operation == 14 {
+                displayResultLabel.text = String(firstNumber - numberFromScreen)
+            }
+            else if operation == 15 {
+                displayResultLabel.text = String(firstNumber * numberFromScreen)
+            }
+            else if operation == 16 {
+                displayResultLabel.text = String(firstNumber / numberFromScreen)
+            }
+        }
+        else if sender.tag == 10 {
+            displayResultLabel.text = ""
+            firstNumber = 0
+            numberFromScreen = 0
+            operation = 0 
         }
     }
     
