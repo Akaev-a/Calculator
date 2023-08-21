c65d6ae85f44990c8f2434a41a67dff1ebd3db9e
Akaev <akaev@MacBook-Pro-Akaev.local>
2023-08-21 21:59:02 +0600

    Add test functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayResultLabel: UILabel!
+    
+    var numberFromScreen: Double = 0
+    var firstNumber: Double = 0
+    var mathsSigh: Bool = false
+    
+    @IBAction func numberPress(_ sender: UIButton) {
+        if mathsSigh == true {
+            displayResultLabel.text = String(sender.tag)
+            mathsSigh = false
+        }
+        else { displayResultLabel.text = displayResultLabel.text! + String(sender.tag)
+        }
+        numberFromScreen = Double(displayResultLabel.text!)!
+    }
+    @IBAction func buttons(_ sender: UIButton) {
+        if displayResultLabel.text != "" {
+            firstNumber = Double(displayResultLabel.text!)!
+            if sender.tag == 10 { // C
+                
+            }
+            else if sender.tag == 11 { // +/-
+                
+            }
+            else if sender.tag == 12 { // %
+                
+            }
+            else if sender.tag == 13 { // +
+                displayResultLabel.text = "+"
+                
+            }
+            else if sender.tag == 14 { // -
+                
+            }
+            else if sender.tag == 15 { // x
+                
+            }
+            else if sender.tag == 16 { // /
+                
+            }
+            else if sender.tag == 17 { // =
+                
+            }
+            else if sender.tag == 18 { // koren`
+                
+            }
+            else if sender.tag == 19 { // .
+                
+            }
+            mathsSigh = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
